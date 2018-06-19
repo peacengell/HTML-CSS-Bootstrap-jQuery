e08d1c339d451e8c3c3301652279b1e6014791bb
manoj.maracheea <manoj.maracheea@redboxdigital.com>
2018-06-19 21:42:41 +0400

    Lesson targetting classes and id's

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
     $("button").addClass("animated bounce");
+    $(".jumbotron").addClass("animated shake");
+    $("#target1").addClass("animated fadeIn");
 
 });
