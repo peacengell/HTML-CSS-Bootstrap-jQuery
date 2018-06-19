264c217a12a1ed257c2c01e0d615be027d5a8334
manoj.maracheea <manoj.maracheea@redboxdigital.com>
2018-06-19 21:51:13 +0400

    Lesson remove add css styling using Jquery.

--- js/app.js
+++ js/app.js
@@ -3,4 +3,7 @@ $(document).ready(function() {
     $("button").addClass("animated");
     $(".btn-default").addClass("tada");
     $("#target1").addClass("btn-primary");
+    $("button").removeClass("btn-default");
+    $("button").removeClass("btn-default");
+    $("#target1").css("color", "red");
 });
