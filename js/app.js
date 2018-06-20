d11fced2c2846878f67901f01ce350b5a0c953b5
manoj.maracheea <manoj.maracheea@redboxdigital.com>
2018-06-20 16:53:12 +0400

    Lesson Targeting child element in jquery

--- js/app.js
+++ js/app.js
@@ -3,5 +3,8 @@ $(document).ready(function() {
     $(".left").clone().appendTo(".right");
 
     $(".row").parent().css("background-color", "green");
+    $(".container").children().css("background-color", "#f6f6f6");
+    $(".target:nth-child(3)").addClass("animated shake");
+
 
 });
