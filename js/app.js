5fd1a72abf5018ed007e06d71ccf6cbdbaadd430
manoj.maracheea <manoj.maracheea@redboxdigital.com>
2018-06-20 16:42:27 +0400

    Lesson  append and remove in Jquery

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
-    $("#target1").prop("disabled", true);
-    $("h2").html("<h2><i>This is Awesome Column</i></h2>");
+    $("#target3").appendTo(".right");
+    $("#target3").remove();
+
 });
